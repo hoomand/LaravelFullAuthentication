0c6a9aa7b9e9e72252bb687fb5e6ca712d32f7c6
Bijan Hoomand <hoomand@gmail.com>
2013-11-26 19:15:48 +0330

    Include first/last name, gender & phone in user profile

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -7,5 +7,28 @@
 
 @section('content')
     <h3>{{ Auth::user()->username }}</h3>
+    
+    <table class="table">
+        <tr>
+            <th>First Name:</th>
+            <td>{{ Auth::user()->first_name }}</td>
+            <th>Last Name:</th>
+            <td>{{ Auth::user()->last_name }}</td>
+        </tr>
+        <tr>
+            <th>Gender:</th>
+            <td>{{ Auth::user()->gender }}</td>
+            <th>Email:</th>
+            <td>{{ Auth::user()->email }}</td>
+        </tr>
+         <tr>
+            <th>Phone:</th>
+            <td>{{ Auth::user()->phone }}</td>
+            <th>Cell Phone:</th>
+            <td>{{ Auth::user()->cellphone }}</td>
+        </tr>
+        
 
+    </table>
+    <a href="{{{ URL::to('profile_edit') }}}" class="btn btn-primary btn-sm active" role="button">Edit Profile</a>
 @stop
