4b737767cd3999fc1d63df394a5fcb61b2ccc628
Bijan Hoomand <hoomand@gmail.com>
2013-11-26 16:32:02 +0330

    Add extra users to user seeder

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -5,7 +5,38 @@ class UserSeeder extends DatabaseSeeder {
             [
                 "username" => "sirbijan",
                 "password" => Hash::make('whatever123'),
-                "email" => "hoomand@gmail.com"
+                "email" => "hoomand@gmail.com",
+                "first_name" => "Bijan",
+                "last_name" => "Hoomand",
+                "cellphone" => "+989357752066",
+                "gender" => "male"
+            ],
+            [
+                "username" => "reza",
+                "password" => Hash::make('rezareza'),
+                "email" => "datairan@gmail.com",
+                "first_name" => "Reza",
+                "last_name" => "Faghihi",
+                "cellphone" => "+989125208474",
+                "gender" => "male"
+            ],
+            [
+                "username" => "ramin",
+                "password" => Hash::make('raminramin'),
+                "email" => "ram.rezazadeh@gmail.com",
+                "first_name" => "Ramin",
+                "last_name" => "Rezazadeh",
+                "cellphone" => "+98",
+                "gender" => "male"
+            ],
+            [
+                "username" => "iman",
+                "password" => Hash::make('imaniman'),
+                "email" => "e_nassirian@yahoo.com",
+                "first_name" => "Iman",
+                "last_name" => "Nasirian",
+                "cellphone" => "+98",
+                "gender" => "female"
             ]
         ];
 
