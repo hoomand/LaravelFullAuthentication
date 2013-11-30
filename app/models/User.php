299cdceb42e713ccfad5b562a961b04a4b2a19e9
Bijan Hoomand <hoomand@gmail.com>
2013-11-30 17:43:06 +0330

    Refactor User&Base models, adding create validation

--- app/models/User.php
+++ app/models/User.php
@@ -11,11 +11,16 @@ class User extends BaseModel implements UserInterface, RemindableInterface {
          *
          * @return array
          */
-        public static $rules = array(
-            'username' => 'required|unique:user|alpha_dash|min:4',
+        public static $create_rules = array(
+            'username' => 'required|unique:user|alpha_num|min:4',
             'password' => 'required|min:6|confirmed',
-            'password_confirmation' => 'required|min:6'
+            'password_confirmation' => 'required|min:6',
+            'email' => 'required|email',
+            'gender' => 'required|in:male,female',
+            'first_name' => 'required|alpha_space',
+            'last_name' => 'required|alpha_space'
         );
+
 	public static $update_rules = array(
                 'email' => 'required|email',
                 'gender' => 'required|in:male,female',
