00af364aca8f23379fdbb7239f07d19d8e9911ff
Bijan Hoomand <hoomand@gmail.com>
2013-12-08 15:38:39 +0330

    Implement ACL on User routes

--- app/routes.php
+++ app/routes.php
@@ -19,13 +19,6 @@ Route::get('login', array('as'=>'login', 'uses'=>'UserController@getLogin'));
 Route::post('login', array('before'=>'csrf', 'uses'=>'UserController@postLogin'));
 Route::get('logout', array('as'=>'logout', 'uses'=>'UserController@getLogout'));
 
-Route::get('user/index', array('as'=>'user/index', 'uses'=>'UserController@indexAction'));
-Route::any('user/create', array('before' => 'allowed:users_create', 'as' => 'user/create', 'uses' => 'UserController@createAction'));
-Route::get('user/edit/{id}', array('uses' => 'UserController@editAction'))->where('id', '[0-9]+');
-Route::post('user/edit/{id}', array('before' => 'csrf', 'uses' => 'UserController@editAction'))->where('id', '[0-9]+');
-Route::post('user/edit/roles/{id}', array('before' => 'csrf', 'uses' => 'UserController@editRole'))->where('id', '[0-9]+');
-Route::any('user/delete/{id}', array('before' => 'allowed:users_delete', 'uses' => 'UserController@deleteAction'))->where('id', '[0-9]+');
-
 Route::any('role/index', array('as' => 'role/index', 'uses' => 'RoleController@indexAction'));
 Route::any('role/edit/{id}', array('uses' => 'RoleController@editAction'));
 Route::any('role/edit/perms/{id}', array('uses' => 'RoleController@editPermission'));
@@ -34,4 +27,12 @@ Route::group(array("before" => "auth"), function()
     {
         Route::get('user/profile', array('as'=>'user/profile', 'uses'=>'UserController@profileAction'));
         Route::any('user/profile/edit', array('as'=>'user/profile/edit', 'uses'=>'UserController@editProfileAction'));
-    });
+
+        Route::get('user/index', array('as'=>'user/index', 'uses'=>'UserController@indexAction'));
+        Route::any('user/create', array('before' => 'allowed:users_create', 'as' => 'user/create', 'uses' => 'UserController@createAction'));
+        Route::get('user/edit/{id}', array('before' => 'allowed:users_edit', 'uses' => 'UserController@editAction'))->where('id', '[0-9]+');
+        Route::post('user/edit/{id}', array('before' => 'csrf|allowed:users_edit', 'uses' => 'UserController@editAction'))->where('id', '[0-9]+');
+        Route::post('user/edit/roles/{id}', array('before' => 'csrf|allowed:roles_edit', 'uses' => 'UserController@editRole'))->where('id', '[0-9]+');
+        Route::any('user/delete/{id}', array('before' => 'allowed:users_delete', 'uses' => 'UserController@deleteAction'))->where('id', '[0-9]+');
+
+   });
