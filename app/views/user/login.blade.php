6e1fa6ffe9cfb0ca6b3219b5781c33ce4613c371
Bijan Hoomand <hoomand@gmail.com>
2013-12-13 13:16:39 +0330

    Implement TWBS3 on Login page

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -9,23 +9,26 @@
     <h2>Login</h2>
     {{ Form::open(array('route' => 'login', 'method' => 'POST')) }}
     {{ Form::token() }}
-    <p>
-        {{ Form::label('username', 'Username') }}<br />
-        {{ Form::text('username', Input::old('username'), array("autofocus" => "True")) }}
-    </p>
-    <p>
-        {{ Form::label('password', 'Password') }}<br />
-        {{ Form::password('password') }}
-    </p>
-    @if ($failedLogins > 2)
-        <p>
-            {{ Form::label('captcha', 'Code') }}<br />
-            {{ Form::text('captcha') }}
-        </p>
-        <p>{{ HTML::image(Captcha::img(), 'Captcha image') }}</p>
-    @endif
-    {{ Form::submit('Login') }}
-    <br />
-    {{ HTML::link('password/request', 'Forgot Password?', array('class' => "btn btn-link")) }}
+    <table class="table">
+        <tr>
+            <td>{{ Form::label('username', 'Username') }}</td>
+            <td>{{ Form::text('username', Input::old('username'), array('class' => 'form-control', 'autofocus' => 'True')) }}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('password', 'Password') }}</td>
+            <td>{{ Form::password('password', array('class' => 'form-control')) }}</td>
+        </tr>
+        @if ($failedLogins > 2)
+            <tr>
+                <td>{{ Form::label('captcha', 'Code') }}</td>
+                <td>{{ Form::text('captcha') }}</td>
+            </tr>
+            <tr>
+                <td colspan="2">{{ HTML::image(Captcha::img(), 'Captcha image') }}</td>
+            </tr>
+        @endif
+    </table>
+    {{ HTML::link('password/request', 'Forgot Password?', array('class' => "btn btn-info btn-sm active pull-right")) }}
+    {{ Form::submit('Login', array('class' => "btn btn-primary btn-sm active pull-right")) }}
     {{ Form::close() }}
 @stop
