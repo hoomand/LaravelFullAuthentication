9c4842c9007471f3bb7236b0c69a17f266e1ef1b
Reza Faghihi <datairan@gmail.com>
2013-12-16 19:53:04 +0330

    Refactor navigation on master layout

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -25,42 +25,51 @@
     </head>
 
     <body>
-        <!-- Navbar -->
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-
-                    {{ HTML::link(Route('home'), 'Home') }}
-                    @if ( !Auth::guest() )
-                        @if ( allowed('users_view') )
-                            {{ HTML::link(Route('user/index'), 'Users') }}
-                        @endif
-                        @if ( allowed('roles_view') )
-                            {{ HTML::link(Route('role/index'), 'Roles') }}
-                        @endif
-                    @endif
-
-                    <div class="nav pull-right">
-                        <ul class="nav">
-                            @if ( Auth::guest() )
-                                <li>{{ HTML::link('login', 'Login') }}</li>
-                            @else
-                                <li>
-                                    {{ HTML::link('logout', 'Logout', array('style' => 'display: inline')) }}
-                                    {{ HTML::link('user/profile', Auth::user()->username, array('style' => 'display: inline; color: #FFFFFF')) }}
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Rasla</a>
             </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"> {{ HTML::link(Route('home'), 'Home') }}</li>
+                    @if ( !Auth::guest() )
+                    @if ( allowed('users_view') )
+                    <li>{{ HTML::link(Route('user/index'), 'Users') }}</li>
+                    @endif
+                    @if ( allowed('roles_view') )
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>{{ HTML::link(Route('role/index'), 'Roles') }}</li>
+                         </ul>
+                    </li>
+                </ul>
+                         @endif
+                      @endif
+                <div class="nav pull-right">
+                    <ul class="nav">
+                        @if ( Auth::guest() )
+                        <li>{{ HTML::link('login', 'Login') }}</li>
+                        @else
+                        <li>
+                            {{ HTML::link('logout', 'Logout', array('style' => 'display: inline')) }}
+                            {{ HTML::link('user/profile', Auth::user()->username, array('style' => 'display: inline; color: #FFFFFF')) }}
+                        </li>
+                        @endif
+                    </ul>
+                </div>
+            </div><!--/.nav-collapse -->
+
         </div>
+    </div>
+
 
         <!-- Container -->
         <div class="container">
